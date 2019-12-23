f04b32f7b7cbc0445dcb9503aac081fe865b2768
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2019-12-23 22:13:06 +0100

    add react

---
+++ assets/js/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Route, Switch, Link, withRouter } from 'react-router-dom';
+
+class Home extends Component {
+
+    render() {
+      return <h1>Hello from React!</h1>;
+    }
+
+  }
+
+export default Home;
