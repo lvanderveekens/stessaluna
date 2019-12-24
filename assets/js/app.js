98b8c80a149c4bdeeae1998e3192d8b4cfc73526
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2019-12-24 13:17:14 +0100

    test api call and render result

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Home from "./components/Home";
+import Home from "./components/home";
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Home/>
-                </div>
-            </BrowserRouter>
-        )
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Home />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -3,10 +3,32 @@ import { Route, Switch, Link, withRouter } from 'react-router-dom';
 
 class Home extends Component {
 
-    render() {
-      return <h1>Hello from React!</h1>;
-    }
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
   }
+
+  componentDidMount() {
+    fetch('/api/posts')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ posts: data })
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    console.log('render');
+    return (
+      <ul>
+        {this.state.posts.map((value, index) =>
+          <li key={index}>{value.description}</li>)
+        }
+      </ul>
+    )
+  }
+}
 
 export default Home;
