5b27a6f7f2c40858071d8260e0bbce15de9b12af
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2020-01-12 09:49:59 +0100

    Title is now set from React

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './home/HomePage';
@@ -8,21 +8,28 @@ import { Provider } from 'react-redux';
 import PrivateRoute from './route/PrivateRoute';
 import NotFoundPage from './404/NotFoundPage';
 import store from './redux/store';
+import Helmet from 'react-helmet';
 
 const App = () => {
 
   return (
-    <Router>
-      <Switch>
-        <PrivateRoute path="/" exact component={HomePage} />
-        <Route path="/login" component={LoginPage} />
+    <Fragment>
+      <Helmet>
+        <title>Luna-app</title>
+        <meta charSet="utf-8" />
+      </Helmet>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/" exact component={HomePage} />
+          <Route path="/login" component={LoginPage} />
 
-        <Route path="/404" component={NotFoundPage} />
-        <Redirect to="/404" />
-      </Switch>
-    </Router>
-  )
-}
+          <Route path="/404" component={NotFoundPage} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    </Fragment>
+  );
+};
 
 ReactDom.render(
   <Provider store={store}>
--- assets/js/nav/NavBar.js
+++ assets/js/nav/NavBar.js
@@ -1,43 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Container } from "react-bootstrap"
-import styles from './NavBar.scss?module'
+import { Navbar, Container } from "react-bootstrap";
+import styles from './NavBar.scss?module';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import axios from '../axios/client'
+import axios from '../axios/client';
 
-const NavBar = ({ authenticated, token }) => {
+const NavBar = ({ authenticated }) => {
 
   const [firstName, setFirstName] = useState("");
 
   useEffect(() => {
     // TODO: move to redux state currentUser?
     axios.get('/api/users/me')
-      .then(res => { setFirstName(res.data.firstName) })
+      .then(res => { setFirstName(res.data.firstName); })
       .catch(console.log);
 
-  }, [authenticated])
+  }, [authenticated]);
 
   return (
     <Navbar className={styles.nav} bg="primary" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Aap</Navbar.Brand>
+        <Navbar.Brand href="/">Luna-app</Navbar.Brand>
         {authenticated && firstName != ""
           ? <Navbar.Text>Signed in as: <a href="#login">{firstName}</a></Navbar.Text>
           : <Link to="/login">Login</Link>}
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
 NavBar.propTypes = {
   authenticated: PropTypes.bool.isRequired,
-  token: PropTypes.string,
 };
 
 const mapStateToProps = state => ({
   authenticated: state.user.authenticated,
-  token: state.user.token,
-})
+});
 
 export default connect(mapStateToProps)(NavBar);
