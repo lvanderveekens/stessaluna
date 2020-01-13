86112b8c3aec27d0e65991e58a480e4c28a24708
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2020-01-13 23:54:51 +0100

    working on image upload design

--- assets/js/post/NewPostForm.js
+++ assets/js/post/NewPostForm.js
@@ -6,7 +6,7 @@ import { Formik } from 'formik';
 import { Button, Form } from 'react-bootstrap';
 import * as yup from 'yup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faUpload, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const schema = yup.object({
   text: yup.string().required("Text is a required field!"),
@@ -16,6 +16,7 @@ const schema = yup.object({
 const NewPostForm = ({ fetchPosts }) => {
 
   const [imageUrl, setImageUrl] = useState("");
+  const [imageHover, setImageHover] = useState(false);
 
   const handleSubmit = (values, { resetForm }) => {
     const formData = new FormData();
@@ -71,19 +72,41 @@ const NewPostForm = ({ fetchPosts }) => {
                 isInvalid={!!errors.file}
                 accept=".jpg,.png"
               />
-              <Form.Label className={styles.imageUpload} htmlFor="image">
-                <div className={styles.imageUploadBox}>
+
+              <div className={styles.imageUpload}>
+                <div
+                  className={styles.imageUploadBox}
+                  onMouseEnter={() => setImageHover(true)}
+                  onMouseLeave={() => setImageHover(false)}
+                >
                   {values.image
                     ? (
-                      <div className={styles.selectedImage} style={{ backgroundImage: `url(${imageUrl})` }} />
-                    ) : (
-                      <div>
-                        <span className={styles.imageUploadIcon}><FontAwesomeIcon icon={faUpload} /></span>
-                        Upload
+                      <div
+                        className={styles.selectedImage}
+                        style={imageHover 
+                          ? { backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url(${imageUrl}` }
+                          : { backgroundImage: `url(${imageUrl})` }
+                        }
+                      >
+                        {imageHover && (
+                          <div className="btn" onClick={() => { setFieldValue("image", null); }}>
+                            <span className={styles.iconWrapper}>
+                              <FontAwesomeIcon icon={faTimesCircle} />
+                            </span>
+                            Remove
+                          </div>
+                        )}
                       </div>
+                    ) : (
+                      <Form.Label className="btn" htmlFor="image">
+                        <span className={styles.iconWrapper}>
+                          <FontAwesomeIcon icon={faUpload} />
+                        </span>
+                        Upload
+                      </Form.Label>
                     )}
                 </div>
-              </Form.Label>
+              </div>
             </Form.Group>
             <Button type="submit">Submit</Button>
           </Form>
