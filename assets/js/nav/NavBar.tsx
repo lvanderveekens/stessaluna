0295df4686d3f15f33cdf267d0e4f077191b4f02
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2020-03-22 10:47:46 +0100

    Working on a better nav

--- assets/js/nav/NavBar.tsx
+++ assets/js/nav/NavBar.tsx
@@ -1,52 +1,94 @@
 import React, { FunctionComponent } from 'react';
-import { Navbar, Container, Dropdown } from "react-bootstrap";
+import { Navbar, Container, Dropdown, Spinner, Nav, NavDropdown } from "react-bootstrap";
 import styles from './NavBar.scss?module';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logOut } from '../user/actions';
 import history from '../history/history';
 import User from '../user/user.interface';
-import CustomToggle from '../dropdown/CustomToggle';
+import { useMediaQuery } from 'react-responsive'
 
 interface Props {
+  loggedIn: boolean
   user?: User
   logOut: () => void
 }
 
-const NavBar: FunctionComponent<Props> = ({ user, logOut }) => {
+const NavBar: FunctionComponent<Props> = ({ loggedIn, user, logOut }) => {
 
-  const handleProfileClick = () => {
-    history.push('/profile');
+  const isMobile = useMediaQuery({ maxWidth: 480 });
+  const isLoadingUser = loggedIn && !user;
+
+  const renderMenu = () => {
+    if (isMobile) {
+      return renderMobileMenu();
+    } else {
+      return renderDesktopMenu();
+    }
+  }
+
+  const renderMobileMenu = () => {
+    return (
+      <>
+        {!loggedIn && (
+          <Link className={styles.link} to="/login">Login</Link>
+        )}
+        {isLoadingUser && (
+          <span style={{ padding: '.25rem .75rem' }}>
+            <Spinner animation="border" variant="warning" size="sm" />
+          </span>
+        )}
+        {user && (
+          <>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ border: 'unset', padding: 'unset' }}>
+              <div className={styles.accountWrapperSmall}>
+                <div className={styles.avatar}>
+                  <img src={user.avatar} />
+                </div>
+              </div>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <Nav.Link href="/login" onClick={() => logOut()}>Logout</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        )}
+      </>
+    );
   };
 
-  const handleLogoutClick = () => {
-    logOut();
-    history.push('/login');
-  };
-
+  const renderDesktopMenu = () => {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {!loggedIn && (
+          <Link className={styles.link} to="/login">Login</Link>
+        )}
+        {isLoadingUser && (
+          <Spinner animation="border" variant="warning" size="sm" />
+        )}
+        {user && (
+          <>
+            <div className={styles.accountWrapperLarge}>
+              <div className={styles.avatar}>
+                <img src={user.avatar} />
+              </div>
+              <span className={styles.userText}>{user.username}</span>
+            </div>
+            <Link className={styles.link} to="/profile">Profile</Link>
+            <Link className={styles.link} to="/login" onClick={() => logOut()}>Logout</Link>
+          </>
+        )}
+      </div>
+    );
+  }
+  
   return (
-    <Navbar className={styles.nav} bg="dark" variant="dark" sticky="top">
+    <Navbar className={styles.nav} bg="dark" variant="dark" sticky="top" expand="lg" collapseOnSelect={true}>
       <Container className={styles.container}>
         <Navbar.Brand className={styles.brand}><Link to="/">Stessaluna</Link></Navbar.Brand>
-        {user
-          ? (
-            <Dropdown className={styles.dropDown}>
-              <Dropdown.Toggle as={CustomToggle} id="something">
-                <div className={styles.accountWrapper}>
-                  <div className={styles.avatar}>
-                    <img src={user.avatar} />
-                  </div>
-                  <span className={styles.userText}>{user.username}</span>
-                </div>
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={handleProfileClick}>Profile</Dropdown.Item>
-                <Dropdown.Item onClick={handleLogoutClick}>Logout</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          )
-          : <Link className={styles.loginLink} to="/login">Login</Link>
-        }
+        {renderMenu()}
       </Container>
     </Navbar>
   );
