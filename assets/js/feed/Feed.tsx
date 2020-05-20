fa77d4f09e606317613fda243b23f5739b8b9a4b
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2020-05-20 19:27:39 +0200

    Got a working css transition

--- assets/js/feed/Feed.tsx
+++ assets/js/feed/Feed.tsx
@@ -1,12 +1,14 @@
 import React, {FunctionComponent, useEffect} from "react"
-import Post from "../post/Post"
 import {connect} from "react-redux"
 import {deletePost, fetchPosts} from "../store/post/actions"
-import moment from "moment"
 import PostInterface from "../post/post.interface"
 import {State} from "../store"
-import FeedPlaceholder from "./placeholder/FeedPlaceholder"
-import FadeIn from "react-fade-in"
+import styles from './Feed.scss?module'
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import FeedPlaceholder from "./placeholder/FeedPlaceholder";
+import moment from "moment";
+import Post from "../post/Post";
+
 
 interface Props {
   loading: boolean
@@ -15,38 +17,38 @@ interface Props {
   deletePost: (id: number) => void
 }
 
-const Feed: FunctionComponent<Props> = ({ loading, posts, fetchPosts, deletePost }) => {
+const Feed: FunctionComponent<Props> = ({loading, posts, fetchPosts, deletePost}) => {
   useEffect(() => {
     fetchPosts()
   }, [])
 
   return (
-    <div id="feed">
-      {loading && <FeedPlaceholder />}
-      {!loading && posts
-        .sort((post, other) => other.id - post.id)
-        .map((post) => (
-          // TODO: how to fade-in only the newly fetched posts...?
-          <Post
-            key={post.id}
-            id={post.id}
-            timestamp={moment(post.createdAt).fromNow()}
-            author={post.author}
-            channel={post.channel}
-            text={post.text}
-            image={post.image}
-            exercise={post.exercise}
-            onDelete={() => deletePost(post.id)}
-            comments={post.comments}
-          />
-        ))
-        // TODO: how to let the newly fetched posts still be a part of 'data' in state
+    <div className={styles.feed}>
 
-        // How do I know when to stop rendering the "more" button? I guess only when the next page is empty...
-        // OR the results is empty
-        // onClick=fetchMorePosts()
-        // loadingMorePosts
-      }
+      {loading && <FeedPlaceholder />}
+
+      {!loading && (
+        <TransitionGroup component={null}>
+          {posts
+            .sort((post, other) => other.id - post.id)
+            .map((post) => (
+              <CSSTransition key={post.id} in={true} appear={true} timeout={1000} classNames="fade">
+                <Post
+                  id={post.id}
+                  timestamp={moment(post.createdAt).fromNow()}
+                  author={post.author}
+                  channel={post.channel}
+                  text={post.text}
+                  image={post.image}
+                  exercise={post.exercise}
+                  onDelete={() => deletePost(post.id)}
+                  comments={post.comments}
+                />
+              </CSSTransition>
+            ))
+          }
+        </TransitionGroup>
+      )}
     </div>
   )
 }
