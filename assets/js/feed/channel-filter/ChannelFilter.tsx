021f1a8fe82addfe24ea19df74268cc46d620a4c
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2020-05-30 08:59:26 +0200

    Toggle checkboxes

--- assets/js/feed/channel-filter/ChannelFilter.tsx
+++ assets/js/feed/channel-filter/ChannelFilter.tsx
@@ -21,15 +21,24 @@ const ChannelFilter: FC<Props> = () => {
   const [showModal, setShowModal] = useState(false)
 
 
+
   const handleModalClose = () => {
     setShowModal(false)
   }
+
+  const toggleCheckbox = (channel) => () => {
+    if (channels.includes(channel)) {
+      setChannels(channels.filter(c => c !== channel))
+    } else {
+      setChannels([...channels, channel])
+    }
+  };
 
   return (
     <div className={styles.channelFilter}>
       <button className={cx(styles.button, {on: channels.length})} onClick={() => setShowModal(true)}>
         {channels.length == 0 && <>Channel</>}
-        {channels.length > 0 && channels.join()}
+        {channels.length > 0 && channels.map((c) => ISO6391.getName(c)).join()}
       </button>
       {showModal && (
         <Modal onClose={handleModalClose}>
@@ -37,20 +46,19 @@ const ChannelFilter: FC<Props> = () => {
           <ModalContent className="mb-3">
             {ISO6391.getLanguages(ISO6391.getAllCodes())
               .sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
-              .map(({ code, name }) => (
-                <div key={code} className={styles.channelCheckboxWrapper}>
-                  <input
-                    value={code}
-                    name={name}
-                    type="checkbox"
-                    checked={false}
-                    onChange={() => {
-                      console.log("CHANGED")
-                    }}
-                  />
-                  {name}
-                </div>
-              ))}
+              .map(({ code, name }) => {
+                return (
+                  <div key={code} className={styles.channelCheckboxWrapper}>
+                    <input
+                      type="checkbox"
+                      value={code}
+                      checked={channels.includes(code)}
+                      onChange={toggleCheckbox(code)}
+                    />
+                    {name}
+                  </div>
+                );
+              })}
           </ModalContent>
           <ModalFooter className={styles.footer}>
             <Button>Reset</Button>
