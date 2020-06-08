8206768e9aaf73d8cd2bf9f63c59436f4ff810e3
Luciano van der Veekens <luciano.van.der.veekens@ahold.com>
2020-06-08 16:45:52 +0200

    WIP: repairing desktop view

--- assets/js/home/login-signup-footer/LoginSignupFooter.tsx
+++ assets/js/home/login-signup-footer/LoginSignupFooter.tsx
@@ -2,6 +2,7 @@ import React, {FC, useEffect, useRef} from "react"
 import styles from "./LoginSignupFooter.scss?module"
 import Button from "../../button/Button";
 import { useHistory } from "react-router-dom"
+import {Col, Container, Row} from "react-bootstrap";
 
 interface Props {
 }
@@ -19,12 +20,18 @@ const LoginSignupFooter: FC<Props> = ({}) => {
 
   return (
     <div className={styles.loginSignupFooter} ref={loginSignupFooterRef}>
-      <Button className={styles.loginButton} variant="transparent" onClick={() => history.push("/login")}>
-        Log in
-      </Button>
-      <Button className={styles.signupButton} variant="light" onClick={() => history.push("/signup")}>
-        Sign up
-      </Button>
+      <Container>
+        <Row className="justify-content-center">
+          <Col className={styles.buttonsWrapper} md={6} lg={6} xl={5}>
+            <Button className={styles.loginButton} variant="transparent" onClick={() => history.push("/login")}>
+              Log in
+            </Button>
+            <Button className={styles.signupButton} variant="light" onClick={() => history.push("/signup")}>
+              Sign up
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
