debb5079088dce3bc617580f50011fd7b863c20f
Luciano van der Veekens <lucianovdveekens@gmail.com>
2020-07-17 16:36:58 +0200

    Rewrite the about page

--- assets/js/about/AboutPage.tsx
+++ assets/js/about/AboutPage.tsx
@@ -17,18 +17,32 @@ const AboutPage: FC = ({}) => {
           <div className={styles.imageWrapper}>
             <img src={meImagePath}/>
           </div>
-          <p>Hi <span className={styles.emoji}>👋🏻</span>, my name is Luciano and I'm the founder of Stessaluna, a social platform for
-            community-driven language learning exercises.</p>
-          <p>Practicing a language on a daily basis is fundamental to improvement and with Stessaluna I intended to
-            create such an environment where people can go to and learn or teach a language together for whatever amount
-            of time you have available on a given day.</p>
-          <p>If you <span className={styles.emoji}>👍🏻</span> what I’m doing and want to share new feature ideas with me or you just want to say Hi, send me
-            an email.</p>
+          <p>
+            Hi <span className={styles.emoji}>👋🏻</span>, my name is Luciano and I'm the founder of Stessaluna, a
+            social platform for community-driven language learning exercises.
+          </p>
+          <p>
+            Taking a couple of minutes each day to practice a language is fundamental to improve over time and with
+            this platform I wanted to create such an environment where people can do just that by scrolling through a
+            feed of exercises.
+          </p>
+          <p>
+            I envisioned this as a place for people to come to after having finished their Duolingo tree and they still
+            want to keep working on their target language by practicing more and improving their vocabulary with the
+            help of others.
+          </p>
+          <p>
+            If you <span className={styles.emoji}>👍🏻</span> what I’m doing and want to share new feature ideas with
+            me or you just want to say Hi, send me an email.
+          </p>
           <div className={styles.mailLinkWrapper}>
             <a href="mailto:info@stessaluna.com"><FontAwesomeIcon icon={faPaperPlane}/> Mail me</a>
           </div>
-          <p>Or if you <span className={styles.emoji}>❤️</span> what I’m doing and want to support development and server costs, you can always buy me a dose
-            of caffeine.</p>
+          <p>
+            Or if you <span className={styles.emoji}>❤️</span> what I’m doing and want to support development and
+            server costs, you can always buy me a dose
+            of caffeine.
+          </p>
           <div className={styles.bmcLinkWrapper}>
             <a className={styles.bmcLink} target="_blank" rel="noopener noreferrer" href="https://www.buymeacoffee.com/lvdveekens">
               <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee"/>
